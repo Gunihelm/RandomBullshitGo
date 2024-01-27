3e018ab4edd6730eacab8338208a68632df6ec22
Volker Werner Mussauer <volker@Volkers-MacBook-Pro.local>
2024-01-27 13:27:36 +0100

    isEnemy

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const isEnemy = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
